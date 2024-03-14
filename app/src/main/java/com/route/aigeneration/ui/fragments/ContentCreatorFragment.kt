07b30dc07d3c08599991b4f961b66621207c81ae
Mohamed Ehab <120674384+moehab03@users.noreply.github.com>
2024-03-14 21:50:54 +0200

    Fix bug.

--- app/src/main/java/com/route/aigeneration/ui/fragments/ContentCreatorFragment.kt
+++ app/src/main/java/com/route/aigeneration/ui/fragments/ContentCreatorFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.route.aigeneration.databinding.FragmentContentCreatorBinding
 import com.route.aigeneration.ui.fragments.idea.IdeaGenerationFragment
+import com.route.aigeneration.ui.fragments.image.ImageGenerationFragment
 
 class ContentCreatorFragment(
     private val replaceFragment: (fragment: Fragment) -> Unit,
